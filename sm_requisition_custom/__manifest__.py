dbae5988a276ee474f2e712c3d2678e74e5194cf
smm-sistemas <sistemas@sanmiguelmadera.com>
2025-10-15 11:44:10 -0600

    Add files via upload

---
+++ sm_requisition_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#-------  -------# 
+{   
+    'name' : "Personalizacion de Requisiciones",
+    'summary': "Modulo de personalizacion para requisiciones",
+    'author': "San Miguel",
+    "version": "1.0.0",
+    'category': 'Customizations',
+    'sequence': 200,
+    'depends':['base','base_setup','employee_purchase_requisition','stock'],
+    'data':[
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/stock_picking_view.xml',
+    ],
+    'demo':[],
+    'qweb':[],
+    'application':True,
+    'installable':True,
+    'auto_install':False,
+}
---
+++ sm_requisition_custom/models/stock_picking.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
+
+class StockPicking(models.Model):
+    _inherit= "stock.picking"
+
+    def button_validate(self):
+        for picking in self:
+            if picking.requisition_order:
+                if picking.location_id.usage == 'transit' and picking.location_dest_id.usage == 'internal' and not picking.backorder_id:
+                    previous_picking = self.env['stock.picking'].search([
+                        ('requisition_order', '=', picking.requisition_order),
+                        ('location_id.usage', '=', 'internal'),
+                        ('location_dest_id.usage', '=', 'transit'),
+                        ('state', '!=', 'cancel'),
+                        ('backorder_id', '=', False),  
+                    ], limit=1)
+                    
+                    if not previous_picking:
+                        raise UserError(_('No se encontró un traslado previo para esta requisición.'))
+
+                    if previous_picking.state != 'done':
+                        raise UserError(_('No puede validar este movimiento hasta que se haya completado el traslado desde Transferencia Origen.'))
+
+                    for move in picking.move_ids_without_package:
+                        prev_move = previous_picking.move_ids_without_package.filtered(lambda m: m.product_id.id == move.product_id.id)
+                        if prev_move:
+                            prev_qty = sum(prev_move.mapped('quantity'))
+                            if move.quantity != prev_qty:
+                                raise UserError(_(
+                                    'La cantidad del producto "%s" (%s) no coincide con la cantidad del traslado previo (%s).'
+                                ) % (move.product_id.display_name, move.quantity, prev_qty))
+                
+                if picking.location_id.usage == 'transit' and picking.location_dest_id.usage == 'internal' and picking.backorder_id:
+                    previous_picking = self.env['stock.picking'].search([
+                        ('requisition_order', '=', picking.requisition_order),
+                        ('location_id.usage', '=', 'internal'),
+                        ('location_dest_id.usage', '=', 'transit'),
+                        ('state', '!=', 'cancel'),
+                        ('backorder_id', '!=', False), 
+                    ], limit=1)
+                    
+                    if not previous_picking:
+                        raise UserError(_('No se encontró un traslado previo para esta requisición!.'))
+
+                    if previous_picking.state != 'done':
+                        raise UserError(_('No puede validar este movimiento hasta que se haya completado el traslado desde Transferencia Origen.!'))
+
+                    for move in picking.move_ids_without_package:
+                        prev_move = previous_picking.move_ids_without_package.filtered(lambda m: m.product_id.id == move.product_id.id)
+                        if prev_move:
+                            prev_qty = sum(prev_move.mapped('quantity'))
+                            if move.quantity != prev_qty:
+                                raise UserError(_(
+                                    'La cantidad del producto "%s" (%s) no coincide con la cantidad del traslado previo (%s).'
+                                ) % (move.product_id.display_name, move.quantity, prev_qty))
+
+        return super(StockPicking, self).button_validate()
+
+
+class StockMove(models.Model):
+    _inherit= "stock.move"
+
+    requisition_order = fields.Char(string='Requisición', readonly=True, copy=False, related='picking_id.requisition_order')
+    usage_origin = fields.Selection(related='picking_id.location_id.usage', string='Uso de Ubicación Origen', readonly=True)
+    usage_dest = fields.Selection(related='picking_id.location_dest_id.usage', string='Uso de Ubicación Destino', readonly=True)
+
+
+    user_can_edit_quantity = fields.Boolean(
+        string="Puede editar cantidad",
+        compute='_compute_user_can_edit_quantity',
+        store=False
+    )
+    
+    @api.depends_context('uid')
+    def _compute_user_can_edit_quantity(self):
+        group_id = "dv_requisition_custom.group_requisition_quantity_manager"
+        has_group = self.env.user.has_group(group_id)
+        for move in self:
+            move.user_can_edit_quantity = has_group
+
+    @api.onchange('quantity')
+    def _check_quantity_done(self):
+        for move in self:            
+            if move.quantity > move.product_uom_qty and move.usage_origin == 'internal' and move.usage_dest == 'transit' and move.requisition_order:                
+                related_move = self.env['stock.move'].search([
+                    ('requisition_order', '=', move.requisition_order),
+                    ('state', 'in', ['draft', 'waiting', 'confirmed']),
+                    ('usage_origin', '=', 'transit'),
+                    ('usage_dest', '=', 'internal'),
+                    ('product_id', '=', move.product_id.id)
+                ], limit=1)
+                if related_move:
+                    related_move.sudo().write({'product_uom_qty': move.quantity})
+
+            if move.quantity > move.product_uom_qty and move.usage_origin == 'transit' and move.usage_dest == 'internal' and move.requisition_order:
+                raise ValidationError(
+                    _("La cantidad realizada (%s) no puede ser mayor que la cantidad demandada (%s) para el producto: %s") %
+                    (move.quantity, move.product_uom_qty, move.product_id.display_name)
+                )
+        
+
+            
+
