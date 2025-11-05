a28a984bb7092c98920c0531420d7a44bff03d11
smm-sistemas <sistemas@sanmiguelmadera.com>
2025-11-05 16:03:23 -0600

    Add files via upload

--- sm_pos_auto_ship_later/models/pos_order.py
+++ sm_pos_auto_ship_later/models/pos_order.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from datetime import datetime
 
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
+
+    # Definir los campos que necesitamos
+    to_ship = fields.Boolean(
+        string='Enviar Después',
+        default=False,
+        help='Si está marcado, la orden se enviará en una fecha posterior'
+    )
+    
+    shipping_date = fields.Date(
+        string='Fecha de Envío',
+        help='Fecha programada para el envío'
+    )
 
     @api.model
     def _order_fields(self, ui_order):
@@ -21,4 +32,16 @@ class PosOrder(models.Model):
         
         return order_fields
 
-     
+    def _prepare_picking_vals(self):
+        """
+        Modificar la preparación de los valores del picking para 
+        asegurar que se generen los 2 movimientos
+        """
+        self.ensure_one()
+        vals = super()._prepare_picking_vals()
+        
+        # Si hay una fecha de envío programada, usarla
+        if self.shipping_date:
+            vals['scheduled_date'] = fields.Datetime.now()
+        
+        return vals
