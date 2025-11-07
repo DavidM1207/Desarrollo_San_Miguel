057ee2fafd285f1065c937c94d95c6c7eae294d4
smm-sistemas <sistemas@sanmiguelmadera.com>
2025-11-07 10:07:03 -0600

    Add files via upload

---
+++ sm_fill_rate_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Fill Rate Report',
+    'version': '17.0.1.0.0',
+    'category': 'Inventory',
+    'depends': ['purchase_requisition'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/fill_rate_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ sm_fill_rate_report/models/fill_rate.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class FillRate(models.Model):
+    _name = 'fill.rate'
+    _description = 'Fill Rate'
